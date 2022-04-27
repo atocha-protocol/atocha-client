fb73db26184388212c68fceef4e7695c097a1018
h12 <630086711@qq.com>
2022-04-27 08:09:17 +0800

    Update puzzle answer

--- src/AtochaClient/PuzzleAnswer.js
+++ src/AtochaClient/PuzzleAnswer.js
@@ -1,41 +1,92 @@
 import React, { useEffect, useState } from 'react';
-import {Form, Input, Grid, Card, Statistic, TextArea, Label} from 'semantic-ui-react';
+import {Form, Input, Grid, Card, Statistic, TextArea, Label, Button} from 'semantic-ui-react';
 
 import {useSubstrate, useSubstrateState} from '../substrate-lib';
 import { TxButton } from '../substrate-lib/components';
 import AnswerList from "./AnswerList";
 import {gql} from "@apollo/client";
 import {useAtoContext} from "./AtoContext";
+import {web3FromSource} from "@polkadot/extension-dapp";
 
 function Main (props) {
-  const { api } = useSubstrateState();
+  const { api, currentAccount } = useSubstrateState();
   const { puzzle_hash, answerList } = props;
   const {apollo_client, gql, puzzleSets: {pubRefresh, updatePubRefresh, tryToPollCheck} } = useAtoContext()
 
   // Puzzle information.
   const [answerTxt, setAnswerTxt] = useState('');
   const [answerExplain, setAnswerExplain] = useState('');
-  const [status, setStatus] = useState(null);
+  const [statusTxt, setStatusTxt] = useState(null);
   useEffect(() => {
   }, [api.query.atochaModule]);
 
 
-  function statusChange (newStatus) {
-    if (newStatus.isFinalized) {
-      const query_str = `
-         query{
-          answerCreatedEvents(filter: {
-            puzzleHash:{
-              equalTo: "${puzzle_hash}"
-            }
-          }){
-            totalCount
-          }
-        } `;
-      tryToPollCheck(query_str, updatePubRefresh, ()=>{}, answerList.length);
-    }else{
+  const getFromAcct = async () => {
+    const {
+      address,
+      meta: { source, isInjected }
+    } = currentAccount;
+    let fromAcct;
+    if (isInjected) {
+      const injected = await web3FromSource(source);
+      fromAcct = address;
+      api.setSigner(injected.signer);
+    } else {
+      fromAcct = null;
     }
+    return fromAcct;
+  };
+
+  async function doAnswerPuzzle() {
+    const fromAcct = await getFromAcct();
+    api.tx.atochaModule
+      .answerPuzzle(puzzle_hash, answerTxt, answerExplain)
+      .signAndSend(fromAcct, {}, ({events = [], status}) => {
+        console.log('Transaction status:', status.type);
+
+        if (status.isInBlock) {
+          console.log('Included at block hash', status.asInBlock.toHex());
+          console.log('Events:');
+
+          events.forEach(({event: {data, method, section}, phase}) => {
+            console.log('事件 \t', phase.toString(), `: ${section}.${method}`, data.toString());
+            if (`${section}.${method}` == 'system.ExtrinsicFailed') {
+              setStatusTxt(`${section}.${method} = ${data.toString()}`)
+            }
+          });
+        } else if (status.isFinalized) {
+          // setStatusTxt(`${statusTxt}\nFinalized`)
+          const query_str = `
+             query{
+              answerCreatedEvents(filter: {
+                puzzleHash:{
+                  equalTo: "${puzzle_hash}"
+                }
+              }){
+                totalCount
+              }
+            } `;
+          tryToPollCheck(query_str, updatePubRefresh, ()=>{}, answerList.length);
+        }
+      });
   }
+
+  // function statusChange (newStatus) {
+  //   if (newStatus.isFinalized) {
+  //     const query_str = `
+  //        query{
+  //         answerCreatedEvents(filter: {
+  //           puzzleHash:{
+  //             equalTo: "${puzzle_hash}"
+  //           }
+  //         }){
+  //           totalCount
+  //         }
+  //       } `;
+  //     tryToPollCheck(query_str, updatePubRefresh, ()=>{}, answerList.length);
+  //   }else{
+  //   }
+  // }
 
   return (
     <Grid.Column width={8}>
@@ -57,20 +108,21 @@ function Main (props) {
           />
         </Form.Field>
         <Form.Field style={{ textAlign: 'center' }}>
-          <TxButton
-              label='Submit'
-              type='SIGNED-TX'
-              setStatus={setStatus}
-              refStatus={statusChange}
-              attrs={{
-                palletRpc: 'atochaModule',
-                callable: 'answerPuzzle',
-                inputParams: [puzzle_hash, answerTxt, answerExplain],
-                paramFields: [true, true, true]
-              }}
-          />
+          {/*<TxButton*/}
+          {/*    label='Submit'*/}
+          {/*    type='SIGNED-TX'*/}
+          {/*    setStatus={setStatus}*/}
+          {/*    refStatus={statusChange}*/}
+          {/*    attrs={{*/}
+          {/*      palletRpc: 'atochaModule',*/}
+          {/*      callable: 'answerPuzzle',*/}
+          {/*      inputParams: [puzzle_hash, answerTxt, answerExplain],*/}
+          {/*      paramFields: [true, true, true]*/}
+          {/*    }}*/}
+          {/*/>*/}
+          <Button onClick={()=>doAnswerPuzzle()}>Submit</Button>
         </Form.Field>
-        <div style={{ overflowWrap: 'break-word' }}>{status}</div>
+        <div style={{ overflowWrap: 'break-word' }}>{statusTxt}</div>
       </Form>
     </Grid.Column>
   );
